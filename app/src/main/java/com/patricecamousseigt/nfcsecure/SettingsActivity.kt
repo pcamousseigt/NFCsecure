f71378d347e777e97c0afe0dc372495c327f4118
Patrice Camousseigt <patrice.camousseigt@gmail.com>
2022-05-31 00:27:22 +0200

    Store preferences data selected by the user

--- app/src/main/java/com/patricecamousseigt/nfcsecure/SettingsActivity.kt
+++ app/src/main/java/com/patricecamousseigt/nfcsecure/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.patricecamousseigt.nfcsecure
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -10,14 +12,28 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
+            supportFragmentManager.beginTransaction().replace(R.id.settings, SettingsFragment()).commit()
         }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            val switchPreferenceCompat = findPreference<SwitchPreferenceCompat>("activation")
+            switchPreferenceCompat?.setOnPreferenceChangeListener { preference, newValue ->
+                Toast.makeText(context, newValue.toString(), Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            val listPreference = findPreference<ListPreference>("duration")
+            listPreference?.setOnPreferenceChangeListener { preference, newValue ->
+                Toast.makeText(context, newValue.toString(), Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
